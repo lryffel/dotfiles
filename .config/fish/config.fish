2f28804cc222f7c322d5d79777b346ebe7453b42
Levi Ryffel <levi.ryffel@gmail.com>
2021-02-20 15:34:28 +0100

    first commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,30 @@
+set fish_greeting
+fish_vi_key_bindings
+
+# path
+set -gx PATH ~/.gem/ruby/2.7.0/bin ~/.cargo/bin ~/.local/bin $PATH
+
+# ls
+alias ls='ls -l --color=auto'
+alias s='ls -la --color=auto'
+
+# Navigation
+alias d='cd'
+
+# Launch programs
+alias v='vim --servername VIM'
+alias vim='vim --servername VIM'
+
+# Cosmetics
+alias arch='archey3'
+
+# git
+alias ga='git add'
+alias g='git status'
+alias gs='git status'
+alias gc='git commit'
+alias gb='git branch'
+alias gp='git push'
+alias gd='git diff'
+alias gP='git pull'
+
