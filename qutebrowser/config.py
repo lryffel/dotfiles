3acb7125e1985befa6f6db7bcf75a29fa046e7d5
Levi Ryffel <levi.ryffel@gmail.com>
2021-02-21 22:07:04 +0100

    colors of qutebrowser

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,6 +1,34 @@
 config.load_autoconfig()
-c.colors.webpage.bg = "#000000"
-c.colors.webpage.prefers_color_scheme_dark = True
+
+# background
+c.colors.webpage.bg = "#000000" # set the default background to black
+c.colors.webpage.prefers_color_scheme_dark = True # accept dark theme from websites that offer it
+
+# statusbar
+c.colors.statusbar.caret.bg = "#000000"
+# c.colors.statusbar.caret.fg
+c.colors.statusbar.caret.selection.bg = "#000000"
+# c.colors.statusbar.caret.selection.fg
+c.colors.statusbar.command.bg = "#000000"
+c.colors.statusbar.command.private.bg = "#000000"
+# c.colors.statusbar.command.prviate.fg
+c.colors.statusbar.insert.bg = "#000000"
+c.colors.statusbar.insert.fg = "#ffffff"
+c.colors.statusbar.normal.bg = "#000000"
+# c.colors.statusbar.normal.fg
+c.colors.statusbar.passthrough.bg = "#000000"
+# c.colors.statusbar.passthrough.fg
+c.colors.statusbar.private.bg = "#000000"
+# colors.statusbar.private.fg
+c.colors.statusbar.progress.bg = "#000000"
+c.colors.statusbar.progress.bg = "#000000"
+c.colors.statusbar.url.error.fg = "#940BAD"
+c.colors.statusbar.url.hover.fg = "#940BAD"
+c.colors.statusbar.url.success.http.fg = "#940BAD"
+c.colors.statusbar.url.success.https.fg = "#940BAD"
+c.colors.statusbar.url.warn.fg = "#aa0000"
 
 # this may break some websites
 c.colors.webpage.darkmode.enabled = True
+
+
