c9e4952624dc6dbab9ca9f26e5998970b6d5773b
Levi Ryffel <levi.ryffel@gmail.com>
2021-02-21 02:32:05 +0100

    formatting

--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,23 @@
 set fish_greeting
 fish_vi_key_bindings
 
-# path
+### path
 set -gx PATH ~/.gem/ruby/2.7.0/bin ~/.cargo/bin ~/.local/bin $PATH
+
+### Navigation
+alias d='cd'
+alias q='exit'
+
+### Launch programs
+#
+# vim
+# --servername VIM for it to work with reverse synctex
+alias v='vim --servername VIM'
+alias vim='vim --servername VIM'
 
 # ls
 alias ls='ls -l --color=auto'
 alias s='ls -la --color=auto'
-
-# Navigation
-alias d='cd'
-alias q='exit'
-
-# Launch programs
-# --servername VIM for it to work with reverse synctex
-alias v='vim --servername VIM'
-alias vim='vim --servername VIM'
 
 # git
 alias ga='git add'
