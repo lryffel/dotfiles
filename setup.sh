bff7b531ef9fe7228f2babcd1b8334e0f4129ba0
Levi Ryffel <levi.ryffel@gmail.com>
2021-03-10 03:57:17 +0100

    cleaned up setup

--- setup.sh
+++ setup.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+# uncomment what you need
 
 # parent directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # install necessary programs
 # sudo pacman -S fish sway swaylock swayidle ranger vim vimb waybar zathura git brightnessctl qutebrowser nvim
 # pip install neovim neovim-remote
 
-# make symlinks
-ln -s -i $DIR/fish ~/.config
-ln -s -i $DIR/ranger ~/.config
-ln -s -i $DIR/sway ~/.config
-ln -s -i $DIR/vimb ~/.config
-ln -s -i $DIR/vimrc ~/.vimrc
-ln -s -i $DIR/waybar ~/.config
-ln -s -i $DIR/zathura ~/.config
-ln -s -i $DIR/alacritty ~/.config
-ln -s -i $DIR/qutebrowser ~/.config
-ln -s -i $DIR/nvim ~/.config
+# make symlinks, e.g., like this. probably best to link files rather than folders whenever possible
+# ln -s -i $DIR/fish ~/.config
+# ln -s -i $DIR/ranger ~/.config
+# ln -s -i $DIR/sway ~/.config
+# ln -s -i $DIR/vimb ~/.config
+# ln -s -i $DIR/vimrc ~/.vimrc
+# ln -s -i $DIR/waybar ~/.config
+# ln -s -i $DIR/zathura ~/.config
+# ln -s -i $DIR/alacritty ~/.config
+# ln -s -i $DIR/nvim ~/.config
