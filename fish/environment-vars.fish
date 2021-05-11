aa931bde63d22f6a097648f2495be93cd006dd15
Levi Ryffel <levi.ryffel@gmail.com>
2021-05-11 15:41:32 +0200

    radicle

--- fish/environment-vars.fish
+++ fish/environment-vars.fish
@@ -1,2 +1,2 @@
-set -gx PATH ~/.gem/ruby/2.7.0/bin ~/.cargo/bin ~/.local/bin $PATH
-set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -gx PATH ~/.gem/ruby/3.0.0/bin ~/.cargo/bin ~/.local/bin ~/.radicle/bin $PATH
+ 
