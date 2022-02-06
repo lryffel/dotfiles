201284058d76bd0847693f2262253cd45b301893
Levi Ryffel <levi.ryffel@gmail.com>
2022-02-06 21:32:40 +0100

    learned about abbrs

--- fish/aliases-public.fish
+++ fish/aliases-public.fish
@@ -1,31 +1,41 @@
 ### Navigation
-alias d='cd'
+abbr d  cd
 alias q='exit'
 alias :q='exit'
 
 ### Launch programs
-#
-# vim
-# --servername VIM for it to work with reverse synctex
-alias v='nvim'
-alias vim='nvim'
+abbr v nvim
+abbr vim nvim
+abbr sv sudo nvim
 
 # ls
 alias s='lsd -l'
 
 # git
-alias ga='git add'
-alias g='git status'
-alias gs='git status'
-alias gc='git commit'
-alias gb='git branch'
-alias gp='git push'
-alias gd='git diff'
-alias gP='git pull'
+abbr ga git add
+abbr g git
+abbr gs git status
+abbr gc git commit
+abbr gb git branch
+abbr gp git push
+abbr gd git diff
+abbr gP git pull
 
 # xterm
 alias xterm="xterm -fa 'Monospace' -fs 18"
-# alias ssh="xterm -fa 'Monospace' -fs 18 -e ssh"
+
+# bluetooth
+abbr bc bluetoothctl
+
+# pacman
+abbr p pacman
+abbr pq pacman -Qi
+abbr sp sudo pacman
+abbr spu sudo pacman -Syu
+abbr spr sudo pacman -Runs
+
+# systemctl
+abbr sc systemctl
 
 # network
 # alias nmtui='nmcli dev wifi rescan && nmtui'
