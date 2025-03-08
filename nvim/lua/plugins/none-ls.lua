0f015b48d75ba6695e72ab7bae37a1243db34e21
Levi Ryffel <levi.ryffel@gmail.com>
2025-03-08 10:21:49 +0100

    undo format on save

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -12,18 +12,6 @@ return {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.completion.spell,
       },
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ async = false })
-            end,
-          })
-        end
-      end,
     })
 
     -- Optional: Map a key for manual formatting
