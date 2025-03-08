7b3e708b42befb54c469f7eee70373e0aa1d5def
Levi Ryffel <levi.ryffel@gmail.com>
2025-03-08 18:48:37 +0100

    typst and ltex_extra

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -29,6 +29,7 @@ return {
           "lua_ls",
           "texlab",
           "ltex",
+          "tinymist",
         },
       })
     end,
@@ -46,6 +47,21 @@ return {
     end,
   },
 
+  -- add ltex_extra for extended dictionaries and additional rules
+  {
+    "barreiroleo/ltex_extra.nvim",
+    ft = { "markdown", "tex" },
+    dependencies = { "neovim/nvim-lspconfig" },
+    config = function()
+      require("ltex_extra").setup {
+        server_opts = {
+          capabilities = capabilities,
+          on_attach = function() end,
+        },
+      }
+    end
+  },
+
   -- configures LSPs
   {
     "neovim/nvim-lspconfig",
@@ -53,10 +69,27 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup(lua_ls_setup)
       lspconfig.texlab.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
       lspconfig.ltex.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
+        on_attach = function()
+          require("ltex_extra").setup()
+        end,
+        settings = {
+          ltex = {
+            -- Integrate ltex_extra’s dictionaries and rules
+            dictionary = require("ltex_extra").dictionary,
+            additionalRules = require("ltex_extra").additionalRules,
+          },
+        },
+      })
+      lspconfig.tinymist.setup({
+        capabilities = capabilities,
+        settings = {
+          exportPdf = "onType",
+          formatterMode = "typstyle",
+        },
       })
 
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
