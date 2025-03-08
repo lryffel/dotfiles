df6154e65017343886f8dfb315242ec91b40e079
Levi Ryffel <levi.ryffel@gmail.com>
2025-03-08 10:32:55 +0100

    move none-ls to lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -9,12 +9,15 @@ local lua_ls_setup = {
 }
 
 return {
+  -- installer for various LSPs, linters, and formatters
   {
     "Williamboman/mason.nvim",
     config = function()
       require("mason").setup()
     end,
   },
+
+  -- ensures that LSPs are installed
   {
     "Williamboman/mason-lspconfig.nvim",
     config = function()
@@ -27,6 +30,8 @@ return {
       })
     end,
   },
+
+  -- ensures that other tools from Mason are installed
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     config = function()
@@ -37,6 +42,8 @@ return {
       })
     end,
   },
+
+  -- configures LSPs
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -48,4 +55,23 @@ return {
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
+
+  -- downloads formatters and linters as if they were LSPs
+  {
+    "nvimtools/none-ls.nvim",
+    requires = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local null_ls = require("null-ls")
+
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.completion.spell,
+        },
+      })
+
+      -- Optional: Map a key for manual formatting
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { buffer = 0 })
+    end,
+  }
 }
