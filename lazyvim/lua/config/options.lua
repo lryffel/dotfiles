3466c62ff142f865fc5f29535340af6f4863541d
Levi Ryffel <levi.ryffel@gmail.com>
2026-07-04 09:20:34 +0200

    slight improvements

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -2,5 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.vimtex_view_method = "zathura_simple"
-vim.opt.spelllang = { "en", "de_ch" } -- Replace with your desired languages
+vim.opt.spelllang = { "en", "de_ch" }
+
+-- these options are togglable with <leader><u>
+vim.opt.wrap = false -- line wrap
+vim.opt.conceallevel = 0 -- conceal
+vim.lsp.handlers["$/progress"] = function() end
