f5129228cfdb9bed4a6167fdc100fa6ab7710b81
Levi Ryffel <levi.ryffel@gmail.com>
2026-07-16 15:48:51 +0200

    fix reverse synctex

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -25,11 +25,9 @@ return {
 
   {
     "lervag/vimtex",
-    ft = { "tex", "bib" },
     init = function()
       vim.g.tex_flavor = "latex"
       vim.g.vimtex_view_method = "zathura"
-      vim.g.vimtex_compiler_progname = "nvr"
       vim.g.vimtex_compiler_latexmk = {
         options = {
           "-shell-escape",
